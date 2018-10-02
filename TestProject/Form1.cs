c24e0b73d3c8761c0073e01bc1717267254c8b98
murtaza2k <43776933+murtaza2k@users.noreply.github.com>
2018-10-02 03:44:39 -0700

    Test2
    tEST2

--- TestProject/Form1.cs
+++ TestProject/Form1.cs
@@ -20,6 +20,7 @@ namespace TestProject
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Test");
+            MessageBox.Show("Hello World");
         }
     }
 }
